7631e89826649b29e610f69d2871dbcbf7567b5a
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-21 16:57:59 -0400

    add key

--- Sample-cocoapods-keys/AppDelegate.swift
+++ Sample-cocoapods-keys/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         let k = SampleCocoapodsKeysKeys()
-        
+        print("key = \(k.buddybuildAPIToken)")
+
         return true
     }
 
